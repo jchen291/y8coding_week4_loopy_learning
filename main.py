2a322fb1e752ef7cd9b9bb5e6b36260b124c7eda
jchen291 <jchen29@kellettschool.com>
2024-09-23 06:53:19 +0000

    loopy loops

--- main.py
+++ main.py
@@ -3,43 +3,63 @@
 ## *UNDERNEATH* EACH MESSAGE!
 
 print("\nTASK 1 HERE\n")
-
-
+for i in range(5):
+    print("bob")
 
 print("\nTASK 2 HERE\n")
-
-
+for j in range(7):
+    print("🍜Noodles"))
+print("🍚 Rice")
 
 print("\nTASK 3 HERE\n")
-
-
+for j in range(3):
+    print("🦖Dinosaur")
+    print("🐘 Elephant")
 
 print("\nTASK 4 HERE\n")
-
+for x in range(6):
+    print("🐶 Dog")
+print("🐱 Cat")
 
 
 print("\nTASK 5 HERE\n")
-
-
+print("🐁 Mouse")
+for x in range(4):
+    print("🐸 Frog")
+print("🐎 Horse")
 
 print("\nTASK 6 HERE\n")
-
+for x in range(2):
+    print("🐶 Dog")
+    print("🐱 Cat")
 
 
 print("\nTASK 7 HERE\n")
-
+for x in range(3):
+    print("🦒 Giraffe")
+    print("🐒 Monkey")
 
 
 print("\nTASK 8 HERE\n")
-
+print("🐯 Tiger")
+print("🦁 Lion")
+print("🐯 Tiger")
 
 
 print("\nTASK 9 HERE\n")
-
+print("🙈 Chimp")              ## before BOTH loops begin
+for x in range(3):	
+    for y in range(3):	
+        print("🦔 Hedgehog")   ## inside the inner loop
+    print("🐮 Cow")            ## outside of the inner loop, but in the outer loop!
+print("🦍 Gorilla")
 
 
 print("\nTASK 10 HERE\n")
-
+for i in range(2):
+    for j in range(2):
+        print("🐍 Snake")
+    print("🦎Lizard")    
 
 
 print("\nTASK 11 HERE\n")
